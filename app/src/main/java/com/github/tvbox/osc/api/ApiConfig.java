91958aec452e28028205300ea3359a8b720aea9d
qweqqeqweqeqwqqq11 <135682671+qweqqeqweqeqwqqq11@users.noreply.github.com>
2023-11-01 13:45:53 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,8 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        //String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get("http://饭太硬.top/tv", "万能影视TV-线路");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
